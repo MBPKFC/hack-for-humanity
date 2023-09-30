8fa55b8ed8c9c751ee005d10bab72ce14cedc5c5
Farrukh Karimov <fkarimov4@gmail.com>
2023-09-30 19:12:00 -0400

    added data

---
+++ client/src/data/questions.js
@@ -0,0 +1,104 @@
+[
+    {
+      "questionText": "Regular cancer screenings are important for early detection, which can lead to more effective treatment options and better outcomes.",
+      "answerOptions": [
+        { "answerText": "True", "isCorrect": true },
+        { "answerText": "False", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "Regular cancer screenings are crucial for detecting cancer at an early and more treatable stage. Early detection often leads to more effective treatment options and better overall outcomes for individuals with cancer."
+    },
+    {
+      "questionText": "Which of the following is a screening method for skin cancer?",
+      "answerOptions": [
+        { "answerText": "Mammogram", "isCorrect": false },
+        { "answerText": "Skin biopsy", "isCorrect": true },
+        { "answerText": "Pap test", "isCorrect": false },
+        { "answerText": "Colonoscopy", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "A skin biopsy involves the removal of a small sample of skin tissue for examination under a microscope. It is a common screening method for skin cancer and aids in the early detection and diagnosis of skin-related abnormalities."
+    },
+    {
+      "questionText": "What is the recommended age to begin regular colorectal cancer screenings for average-risk individuals?",
+      "answerOptions": [
+        { "answerText": "30", "isCorrect": false },
+        { "answerText": "40", "isCorrect": false },
+        { "answerText": "50", "isCorrect": true },
+        { "answerText": "60", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "Regular colorectal cancer screenings are typically recommended for average-risk individuals starting at the age of 50. However, individuals with specific risk factors may need to start screening earlier."
+    },
+    {
+      "questionText": "Which of the following is a recommended screening method for prostate cancer?",
+      "answerOptions": [
+        { "answerText": "PSA blood test", "isCorrect": true },
+        { "answerText": "Mammogram", "isCorrect": false },
+        { "answerText": "Pap test", "isCorrect": false },
+        { "answerText": "Colonoscopy", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "The PSA (Prostate-Specific Antigen) blood test measures the level of PSA in the blood. Elevated PSA levels may indicate a higher risk of prostate cancer, prompting further evaluation."
+    },
+    {
+      "questionText": "Mammograms are a recommended screening method for breast cancer.",
+      "answerOptions": [
+        { "answerText": "True", "isCorrect": true },
+        { "answerText": "False", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "Mammograms use low-energy X-rays to examine the breast for early signs of cancer. They are an essential tool for early detection of breast cancer, especially in women over 40."
+    },
+    {
+      "questionText": "The Pap test is used to screen for cervical cancer.",
+      "answerOptions": [
+        { "answerText": "True", "isCorrect": true },
+        { "answerText": "False", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "The Pap test, also known as a Pap smear, is a screening procedure that checks for abnormal changes in the cells of the cervix. It is a crucial tool in detecting and preventing cervical cancer."
+    },
+    {
+      "questionText": "Colonoscopies are a common screening method for colorectal cancer.",
+      "answerOptions": [
+        { "answerText": "True", "isCorrect": true },
+        { "answerText": "False", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "A colonoscopy is a procedure in which a thin, flexible tube with a camera is inserted into the rectum to examine the colon. It is one of the most effective methods for early detection and prevention of colorectal cancer."
+    },
+    {
+      "questionText": "At what age should individuals typically start getting regular mammograms for breast cancer screening?",
+      "answerOptions": [
+        { "answerText": "30", "isCorrect": false },
+        { "answerText": "40", "isCorrect": false },
+        { "answerText": "50", "isCorrect": true },
+        { "answerText": "60", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "Regular mammograms for breast cancer screening are generally recommended for women starting at the age of 50. However, it's important to consult with a healthcare provider, as individual circumstances may vary."
+    },
+    {
+      "questionText": "How often is it recommended to have a Pap test for cervical cancer screening?",
+      "answerOptions": [
+        { "answerText": "Every year", "isCorrect": false },
+        { "answerText": "Every three years", "isCorrect": true },
+        { "answerText": "Every five years", "isCorrect": false },
+        { "answerText": "Every ten years", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "For most women, it is recommended to have a Pap test every three years. However, the frequency may vary based on individual risk factors and healthcare provider recommendations."
+    },
+    {
+      "questionText": "Which of the following is a screening method for lung cancer in individuals with a high risk of the disease?",
+      "answerOptions": [
+        { "answerText": "Chest X-ray", "isCorrect": false },
+        { "answerText": "MRI", "isCorrect": false },
+        { "answerText": "CT scan", "isCorrect": true },
+        { "answerText": "Ultrasound", "isCorrect": false }
+      ],
+      "hint": "",
+      "additionalInfo": "A CT (Computed Tomography) scan is used for lung cancer screening in individuals at high risk, such as heavy smokers. It provides detailed images of the lungs to detect early signs of cancer."
+    }
+  ]
