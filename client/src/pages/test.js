a58b395d367c5a805b72204a4c01b62293f569b9
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 15:15:08 -0400

    reformatting

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -1,14 +1,15 @@
-import { getSingleUser } from "../utils/API";
+import { getSingleUser, createUser } from "../utils/API";
 
 export default function Home() {
+  // example fetch of how to get a single user
   const handleGetSingleUser = async (e) => {
     e.preventDefault();
 
-    try {
-      // needed data for search
-      const userId = "651858eac39aa9822b6f0cc6";
+    // needed data for search
+    const userData = { id: "651858eac39aa9822b6f0cc6" };
 
-      const res = await getSingleUser({ id: userId });
+    try {
+      const res = await getSingleUser({ id: userData.id });
       if (!res.ok) {
         throw new Error("something went wrong!");
       }
@@ -19,6 +20,9 @@ export default function Home() {
       console.error(err);
     }
   };
+
+  // example fetch of how to create a user
+  const handleCreateUser = async (e) => {};
 
   return (
     <main>
