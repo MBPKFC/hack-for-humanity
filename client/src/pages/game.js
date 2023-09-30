99d5e35c87de03595b8876a4d652b0afaa08fd83
Farrukh Karimov <fkarimov4@gmail.com>
2023-09-30 13:54:27 -0400

    basic game route

---
+++ client/src/pages/game.js
@@ -0,0 +1,11 @@
+import Question from "@/components/Question";
+import React, { useContext } from "react";
+
+export default function Game() {
+//   const { currentStep } = useContext();
+  return (
+    <div>
+      <Question />
+    </div>
+  );
+}
