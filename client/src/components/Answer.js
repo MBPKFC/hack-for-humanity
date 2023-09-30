4e4b8960c51b0e15fa28cbe70b4d57859d5f2221
ken862734801 <kenshawmichaud@gmail.com>
2023-09-30 16:46:10 -0400

    add random text functionality

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -1,12 +1,20 @@
-
+import { useEffect, useState } from "react";
 const correctOptions = ["Correct!", "You're a genius!"];
-const incorrectOptions = ["Not Quite.", "That was a tough one."];
+const incorrectOptions = ["Practice makes perfect.", "That was a tough one."];
 
 export default function Answer (props){
+    const [randomCorrectText, setRandomCorrectText] = useState();
+    const [randomIncorrectText, setRandomIncorrectText] = useState();
+
+    useEffect(() => {
+        setRandomCorrectText(correctOptions[Math.floor(Math.random() * correctOptions.length)]);
+        setRandomIncorrectText(incorrectOptions[Math.floor(Math.random() * incorrectOptions.length)]);
+    });
+
     return (
         <div>
             <div>
-                <p>{ props.isCorrect?  correctOptions[0] : incorrectOptions[0] }</p>
+                <p>{ props.isCorrect?  `${randomCorrectText}` : `${randomIncorrectText}` }</p>
                 <p>{ props.additionalInformation }</p>
             </div>
             <div>
