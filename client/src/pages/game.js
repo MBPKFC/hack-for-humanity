84f751a8e3d22ce11ff50fbb7daa6e12419fb862
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-09-30 19:54:29 -0400

    updated game.js with container styles

--- client/src/pages/game.js
+++ client/src/pages/game.js
@@ -1,27 +1,28 @@
 import Question from "@/components/Question";
 import { StepContext } from "@/context/StepContext";
+import { Container } from "@/library/Container";
 import React, { useContext, useState } from "react";
 import Progress from "../components/Progress";
 
 export default function Game() {
-  const [showAnswer, setShowAnswer] = useState(false);
-  const { isAnswerCorrect, setIsAnswerCorrect } = useContext(StepContext);
+   const [showAnswer, setShowAnswer] = useState(false);
+   const { isAnswerCorrect, setIsAnswerCorrect } = useContext(StepContext);
 
-  console.log(isAnswerCorrect);
+   console.log(isAnswerCorrect);
 
-  return (
-    <div className="bg-brand-tan h-screen">
-      <Progress />
-      {!showAnswer ? (
-        <Question
-          setIsAnswerCorrect={setIsAnswerCorrect}
-          setShowAnswer={setShowAnswer}
-        />
-      ) : (
-        <div>
-          Your answer is {isAnswerCorrect ? "correct! 😁" : "incorrect! 😢"}
-        </div>
-      )}
-    </div>
-  );
+   return (
+      <Container className="bg-brand-tan h-screen">
+            {!showAnswer ? (
+               <Question
+                  setIsAnswerCorrect={setIsAnswerCorrect}
+                  setShowAnswer={setShowAnswer}
+               />
+            ) : (
+               <div>
+                  Your answer is{" "}
+                  {isAnswerCorrect ? "correct! 😁" : "incorrect! 😢"}
+               </div>
+            )}
+      </Container>
+   );
 }
