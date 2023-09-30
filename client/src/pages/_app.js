7bcf7b287b88502a28ba64c86a6f35df58c57c95
Farrukh Karimov <fkarimov4@gmail.com>
2023-09-30 16:01:05 -0400

    built Question component

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { StepContextProvider } from "@/context/StepContext";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StepContextProvider>
+      <Component {...pageProps} />
+    </StepContextProvider>
+  );
 }
--- client/src/pages/game.js
+++ client/src/pages/game.js
@@ -1,11 +1,23 @@
 import Question from "@/components/Question";
-import React, { useContext } from "react";
+import { StepContext } from "@/context/StepContext";
+import React, { useContext, useState } from "react";
 
 export default function Game() {
-//   const { currentStep } = useContext();
+  const [showAnswer, setShowAnswer] = useState(false);
+  const { isAnswerCorrect, setIsAnswerCorrect } = useContext(StepContext);
+
+  console.log(isAnswerCorrect);
+
   return (
-    <div>
-      <Question />
+    <div className="bg-brand-tan h-screen">
+      {!showAnswer ? (
+        <Question
+          setIsAnswerCorrect={setIsAnswerCorrect}
+          setShowAnswer={setShowAnswer}
+        />
+      ) : (
+        <div>Your answer is {isAnswerCorrect? 'correct! 😁' : 'incorrect! 😢'}</div>
+      )}
     </div>
   );
 }
