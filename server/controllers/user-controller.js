4ff58c6ee7c0d2fa9e79d178d2e804c35a96c2c4
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 13:16:20 -0400

    added getAllUsers, getSingleUser, and createUser

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,46 @@
+// import user model
+const { User } = require("../models");
+
+module.exports = {
+  // get all users
+  async getAllUsers(req, res) {
+    try {
+      const allUsers = await User.find().select("-__v");
+
+      res.json(allUsers);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // get single user
+  async getSingleUser(req, res) {
+    try {
+      const dbUserData = await User.findOne({ _id: req.params.id }).select(
+        "-__v"
+      );
+
+      if (!dbUserData) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // create user
+  // example input can be found in server/seeds/userSeeds.json
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
