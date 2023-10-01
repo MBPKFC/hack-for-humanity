f26a67b8b6ead4ab02d6c9886a644d6d950e2dbc
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 20:58:25 -0400

    made edits to server to allow vercel to deploy

--- server/index.js
+++ server/index.js
@@ -8,17 +8,23 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/.next')));
 }
 
 app.use(routes);
+app.use("/home", (req, res) => {
+  res.send("Server is running")
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
