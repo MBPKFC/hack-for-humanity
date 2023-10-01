ab6631f740fc44381c83b54490cf6b2e634b9dc7
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 23:10:59 -0400

    user created on play click, redirect to game

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,39 @@
 import Image from "next/image";
 import { Button } from "@/library/Button";
 import { Container } from "@/library/Container";
+import { useRouter } from 'next/router'
+
+import { createUser } from "../utils/API";
 
 export default function Home() {
+  const router = useRouter()
+
+  const clickHandler = async (e, url) => {
+    e.preventDefault();
+
+    // is userId already exists in local storage then continue to page redirect
+    if (localStorage.getItem("userId")) return pageRedirect(url);
+
+    try {
+      const res = await createUser({});
+      if (!res.ok) {
+        throw new Error("something went wrong!");
+      }
+
+      const user = await res.json();
+      console.log(user);
+      localStorage.setItem("userId", user.id);
+    } catch (err) {
+      console.error(err);
+    }
+     pageRedirect(url);
+  };
+
+  const pageRedirect = (url) => {
+   console.log(url)
+   router.push(`/${url}`)
+  };
+
   return (
     <Container className="flex flex-col items-center justify-between font-semibold">
       <div className="mb-12 mt-6">
@@ -20,7 +51,9 @@ export default function Home() {
       <h3 className="mt-6 max-w-[280px] text-center text-lg">
         Learn about health screenings and win prizes.
       </h3>
-      <Button className="mt-6 w-full">Play</Button>
+      <Button onClick={(e) => clickHandler(e, "game")} className="mt-6 w-full">
+        Play
+      </Button>
       <p className="mt-6">Sponsored by</p>
       <Image
         src="/images/american-cancer-society-logo-1.png"
