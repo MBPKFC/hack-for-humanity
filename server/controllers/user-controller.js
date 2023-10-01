6320542adf3cc8d177828a543004e9c0848b42d2
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-10-01 06:29:38 -0400

    edited res data for getVIPData

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -70,22 +70,6 @@ module.exports = {
     try {
       const allUsers = await User.find().select("-__v");
 
-      const zipCount = {};
-      const providerCount = {};
-
-      allUsers.forEach((ele) => {
-        if (zipCount[ele.zip]) {
-          zipCount[ele.zip] += 1;
-        } else {
-          zipCount[ele.zip] = 1;
-        }
-        if (providerCount[ele.insurance.provider]) {
-          providerCount[ele.insurance.provider] += 1;
-        } else {
-          providerCount[ele.insurance.provider] = 1;
-        }
-      });
-
       const ag = await User.aggregate([
         {
           $group: {
@@ -96,17 +80,12 @@ module.exports = {
         {
           $sort: { count: -1 },
         },
-        {
-          $limit: 1,
-        },
       ]);
 
       res.json({
-        zipCount,
-        providerCount,
         totalUsers: allUsers.length,
-        topZip: ag[0]._id,
-        // topProvider,
+        topZip: {zip: ag[0]._id, count: ag[0].count},
+        zipCount: ag,
       });
     } catch (err) {
       console.log(err);
