0e6d7d4c24534ccd8c8ae70f2bafe6b0d49b597e
Farrukh Karimov <fkarimov4@gmail.com>
2023-10-01 01:41:47 -0400

    updated Progress bug

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -8,23 +8,23 @@ const Progress = () => {
     <div className="flex justify-center py-4">
       {questions.map((question, index) => {
         return index > currentStep ? (
-          <>
+          <React.Fragment key={index}>
             <div className="flex h-6 w-6 items-center justify-center rounded-full border border-brand-blue bg-white text-xs font-bold text-brand-blue">
               {index + 1}
             </div>
             {index !== questions.length - 1 && (
               <span className="h-[2px] w-4 self-center bg-brand-blue"></span>
             )}
-          </>
+          </React.Fragment>
         ) : (
-          <>
+          <React.Fragment key={index}>
             <div className="flex h-6 w-6 items-center justify-center rounded-full bg-brand-blue text-xs font-bold text-white ">
               {index + 1}
             </div>
             {index !== questions.length - 1 && (
               <span className="h-[2px] w-4 self-center bg-brand-blue"></span>
             )}
-          </>
+          </React.Fragment>
         );
       })}
     </div>
