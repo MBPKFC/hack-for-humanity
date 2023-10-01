f8eeeed4da38fbe9d625416d72f0deed7cf2c544
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-10-01 05:15:49 -0400

    restructured user routes so overlap doesnt occur

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -4,15 +4,16 @@ const {
   getSingleUser,
   getAllUsers,
   updateSingleUser,
-  getVIPData
+  getVIPData,
 } = require("../../controllers/user-controller");
 
 // /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
-// /api/users/:id    ex. api/users/123
-// router.route("/:id").get(getSingleUser).put(updateSingleUser);
+// /api/users/data
+router.route("/data").get(getVIPData);
 
-router.route("/data").get(getVIPData)
+// /api/users/:id    ex. api/users/123
+router.route("/:id").get(getSingleUser).put(updateSingleUser);
 
 module.exports = router;
