87c9ac38b8f422cc4f5acf19ecf28d4be033eda5
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2023-10-01 02:50:06 -0400

    added dog to header after homeview

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,27 @@
+import { useContext } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { StepContext } from "@/context/StepContext";
+import { cn } from "@/utils/cn";
 
 export const Header = () => {
+  const router = useRouter();
+  console.log("router: ", router.route);
+
   return (
-    <div className="flex justify-between bg-brand-blue text-white py-2 px-4">
+    <div className="flex justify-between bg-brand-blue px-4 py-2 text-white">
       <Link href="/">
-        <Image
-          src="/acs-logo.svg"
-          alt="American Cancer Society Logo"
-          width={80}
-          height={80}
-        />
+        <Image src="/acs-logo.svg" alt="American Cancer Society Logo" width={80} height={80} />
       </Link>
-      {/* <button className="py-2 px-4 rounded-full font-bold bg-white text-[#ff0000]">Get Screened</button> */}
+      <Image
+        src="/images/img-6456-photo-room-11.png"
+        alt="hero mascot"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className={cn(["max-h-11 w-min", { hidden: router.route === "/" }])}
+      />
     </div>
   );
 };
