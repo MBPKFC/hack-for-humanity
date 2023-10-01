6b2a7581edfcd77127003c348e640d7738769a9e
ken862734801 <kenshawmichaud@gmail.com>
2023-10-01 01:39:10 -0400

    update provider card styling

--- client/src/components/ProviderCard.js
+++ client/src/components/ProviderCard.js
@@ -1,7 +1,7 @@
 const ProviderCard = (props) => {
     return (
         <div className="bg-white my-4 rounded-md shadow-lg p-4">
-            <p className="font-bold rounded-lg">{props.name}</p>
+            <p className="font-bold">{props.name}</p>
             <p>{props.address}</p>
             <p>{props.contact}</p>
             <p>{props.distance}</p>
